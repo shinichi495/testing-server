e24b480c2e7010a9331a0c07efb965a99bfda023
nymph <namph2501@gmail.com>
2019-03-08 14:12:00 +0700

    example

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-const a;
-const b;
+const {ApolloServer,gql} =  require('apollo-server') 
+const schema = gql`
+    type Query {
+        me : User
+    }
+    type User {
+        user : String!
+        pass : String!
+    }
+` ;
+const resolver = {
+    Query: {
+        me : () => {
+            return {user : "shinichi495", pass : "13232"}
+        }
+    }
+};
+const server = new ApolloServer({
+    typeDefs : schema,
+    resolvers : resolver
+});
+server.listen().then(({url}) => {
+    console.log(`server start at ${url}`);
+})
